0d6c47ba0474b5a4f3b809d931f377793dec2fb0
anggaid14 <37369406+anggaid14@users.noreply.github.com>
2018-03-19 16:42:11 +0700

    Update like.php

--- like.php
+++ like.php
@@ -3,16 +3,16 @@ function autolike($link, $jml){
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL,"http://sandromau.tk/");
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS,"nohp=".$link."&jml=".$jml);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,"link=".$link."&jml=".$jml);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
         curl_setopt($ch, CURLOPT_REFERER, 'http://sandromau.tk/');
         curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
         $server_output = curl_exec ($ch);
         curl_close ($ch);
-		echo "SUKSES & Follow @yaelahngga_";
+		echo "SUCCESS & Follow @yaelahngga_";
         flush();
 		
     }
